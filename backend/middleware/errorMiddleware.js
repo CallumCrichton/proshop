3e779bd9c9cebce01032c8248d38d0ca7b1e0051
Callum Crichton <callumcrichton330@gmail.com>
2024-12-08 23:16:44 +0000

    product controller

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,23 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (error, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = error.message;
+
+  // Check for CastError
+  if (err.name === "CastError" && err.kind === "ObjectID") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
