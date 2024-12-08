b5cd241c440103b07da60a5fc5d66a02672966e7
Callum Crichton <callumcrichton330@gmail.com>
2024-12-08 21:35:50 +0000

    data modelling

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,16 @@ connectDB(); // Connect to MongoDB
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("API is running...");
+  res.send("API is running...");
 });
 
 app.get("/api/products", (req, res) => {
-    res.json(products);
+  res.json(products);
 });
 
 app.get("/api/products/:id", (req, res) => {
-const product = products.find((p) => p._id === req.params.id);
-res.json(product);
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
